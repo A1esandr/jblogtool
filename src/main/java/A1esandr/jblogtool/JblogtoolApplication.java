1b85e476078e11c4738404cd06a3f867761d0743
kurakin.as <aleksandr.kurakin@domru.ru>
2020-09-04 16:45:47 +0500

    Init project

---
+++ src/main/java/A1esandr/jblogtool/JblogtoolApplication.java
@@ -0,0 +1,13 @@
+package A1esandr.jblogtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblogtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JblogtoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/A1esandr/jblogtool/JblogtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package A1esandr.jblogtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JblogtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
